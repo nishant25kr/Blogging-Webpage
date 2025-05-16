092ea6b962d193b2ed979397e77dd2228087f536
Nishant kumar <nishant927472@gmail.com>
2025-05-16 11:11:34 +0530

    cpmpleted

--- Bolgging-Webpage/src/App.jsx
+++ Bolgging-Webpage/src/App.jsx
@@ -1,13 +1,78 @@
+// import './App.css'
+// import React,{useState,useEffect} from "react"
+// import { useDispatch } from 'react-redux'
+// import authService from "./appwrite/auth"
+// import {Header, Footer} from "./Components"
+
+
+// function App() {
+//   // console.log(import.meta.env.VITE_APPWRITE_URL)
+//   useEffect(()=>{
+//     authService.getCurrentUser()
+//     .then((userData)=>{
+//       if(userData){
+//         dispatch(login({userData}))
+//       }
+//       else{
+//         dispatch(logout())
+//       }
+//     })
+//     .finally(()=>setLoading(false))
+//   },[])
+
+//   return !loading? (
+//     <div className='min-h-screen flex flex-wrap content-between bg-green '>
+//       <div className='w-fill block'>
+//         <Header/>
+//         <main>
+//           //outlet is there
+          
+//         </main>
+//         <Footer/>
+//       </div>
+//     </div> 
+//   ) : (null)
+// }
+
+// export default App
+
+
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+import { Footer, Header } from './components'
+// import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>blogging app</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+  
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        {/* TODO:  <Outlet /> */}
+        outlet
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
